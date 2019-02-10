c6b1e58ad47f0331175bc4f041b44179ddd335b3
yann <yann.boisselier@gmail.com>
2019-02-10 18:03:18 +0100

    first commit

---
+++ gridsome.config.js
@@ -0,0 +1,18 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here requires a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: "La maison sur la place",
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "content/homeSections/*.md",
+        typeName: "HomeSection"
+      }
+    }
+  ]
+};
---
+++ src/pages/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135"/>
+    <h1>{{$page.metaData.siteName}}</h1>
+    <div v-html="$page.header.content"/>
+  </Layout>
+</template>
+
+<page-query>
+query Home {
+  metaData {
+    siteName
+  }
+  header: homeSection(path: "/content/home-sections/header") {
+    title
+    content
+  }
+  test: homeSection(path: "/content/home-sections/test") {
+    title
+    content
+  }
+}
+</page-query>
+
+
+<script>
+export default {
+  // ..
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
